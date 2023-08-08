7c92db27aa78d75e3e86e08d09c44d7440304b76
Riel <jasper.apos@gmail.com>
2023-08-08 17:01:21 +0800

    Binary Tree

--- binaryTree.c
+++ binaryTree.c
@@ -38,7 +38,7 @@ treePtr insertNode() {
 
         printf("Inserting node %d\n", temp->elem);
 
-        // Insert either left and right using recursion
+        // Insert either left or right using recursion
         printf("Inserting %d's left subtree\n", temp->elem);
         temp->left = insertNode();
         printf("Inserting %d's right subtree\n", temp->elem);
