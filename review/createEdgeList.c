69667c1cf4104498b2d102a13038af82db5c0e18
Riel <jasper.apos@gmail.com>
2024-01-22 18:02:35 +0800

    review: undirected graphs (adjacency matrix)

---
+++ review/createEdgeList.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SENTINEL 999
+#define MAX_VERTEX 4
+#define ARRAY_SIZE 20
+
+typedef struct {
+  int u, v;
+  int weight;
+} edgeType;
+
+typedef struct node {
+  edgeType edges[ARRAY_SIZE];
+  int lastEdge;
+} *edgeList;
+
+typedef int adjMatrix[MAX_VERTEX][MAX_VERTEX];
+
+edgeList createEdgeList(adjMatrix M);
+void displayEdgeList(edgeList L);
+
+int main() {
+  adjMatrix M = {
+    { SENTINEL, 16, 1, 8 }, 
+    { 16, SENTINEL, 15, 24 }, 
+    { 1, 15, SENTINEL, 30 }, 
+    { 8, 24, 30, SENTINEL }
+  };
+
+  edgeList myEdgeList = NULL;
+  myEdgeList = createEdgeList(M);
+
+  displayEdgeList(myEdgeList);
+  printf("last index: %d\n", myEdgeList->lastEdge);
+
+  return 0;
+}
+
+edgeList createEdgeList(adjMatrix M) {
+  edgeList retList = malloc(sizeof(struct node));
+  retList->lastEdge = -1;
+  int row, col, indx;
+
+  for (row = 0; row < MAX_VERTEX; row++) {
+    for (col = 0; col < MAX_VERTEX; col++) {
+      if (M[row][col] != SENTINEL && row > col) {
+        if (retList->lastEdge < ARRAY_SIZE - 1) {
+          for (indx = retList->lastEdge; indx >= 0 && retList->edges[indx].weight > M[row][col]; indx--) {
+            retList->edges[indx + 1] = retList->edges[indx];
+          }
+          edgeType temp;
+          temp.v = row;
+          temp.u = col;
+          temp.weight = M[row][col];
+
+          retList->edges[indx + 1] = temp;
+          retList->lastEdge++;
+        }
+      }
+    }
+  }
+  return retList;
+}
+
+void displayEdgeList(edgeList L) {
+  int indx;
+  for (indx = 0; indx <= L->lastEdge; indx++) {
+    printf("(%d, %d): %d\n", L->edges[indx].u, L->edges[indx].v, L->edges[indx].weight);
+  }
+}
